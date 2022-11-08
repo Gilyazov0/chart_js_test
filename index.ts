6755e56644e01c96056e1d4cd723be19636cd708
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-11-08 13:19:26 +0200

    working with mistake in typescript

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { Chart } from "chart.js";
-
+// import { Chart } from "chart.js";
+// "https://cdn.jsdelivr.net/npm/chart.js";
 const body = document.getElementsByTagName("body")[0]!;
 body.style.background = "red";
 
